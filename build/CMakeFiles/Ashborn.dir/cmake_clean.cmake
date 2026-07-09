7a8e4c8d9df876e043b8e0d4b9bb262394503179
Hasegan Tudor-Mihai <next15740@yahoo.com>
2026-07-10 02:27:21 +0300

    batman

---
+++ build/CMakeFiles/Ashborn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ashborn.dir/link.d"
+  "Ashborn"
+  "Ashborn.pdb"
+  "CMakeFiles/Ashborn.dir/src/main.cpp.o"
+  "CMakeFiles/Ashborn.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ashborn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Ashborn.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Ashborn.
+# This may be replaced when dependencies are built.
